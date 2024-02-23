9acd0f161b1ba9a925f410df12050e418c27a96b
GwonSuhyeon <suhyeon7175@naver.com>
2024-02-24 02:05:04 +0900

    2024.02.24

---
+++ BOJ/2164.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+n = int(input())
+q = deque()
+
+for i in range(1, n + 1):
+    q.append(i)
+
+while len(q) != 1:
+    
+    q.popleft()
+    
+    if len(q) == 1:
+        break
+    
+    q.append(q.popleft())
+
+print(q[0])
