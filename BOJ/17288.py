af849fbe6b38819618fb2c6e5464114ea94ce6aa
GwonSuhyeon <suhyeon7175@naver.com>
2024-06-14 00:38:51 +0900

    2024.06.14

--- BOJ/17288.py
+++ BOJ/17288.py
@@ -1,25 +1,3 @@
-# S = input()
-
-# res = 0
-# idx = 0
-
-# while idx <= (len(S) - 3):
-#     if int(S[idx]) == int(S[idx + 1]) - 1 and int(S[idx + 1]) == int(S[idx + 2]) - 1:
-#         res += 1
-        
-#         cnt = 0
-#         while (idx + 3) < len(S) and int(S[idx + 1]) == int(S[idx + 2]) - 1 and int(S[idx + 2]) == int(S[idx + 3]) - 1:
-#             idx += 1
-#         idx += 3
-#         # continue
-    
-#     idx += 1
-
-# print(res)
-
-
-
-
 S = input()
 
 num = None
@@ -39,10 +17,7 @@ for i in S:
             if len(arr) == 3:
                 res += 1
             
-            # else:
-            # num = None
             arr.clear()
-            
             arr.append(int(i))
         
         num = int(i)
