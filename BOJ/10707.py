b170c186693be7da336c71635637eb508704a96e
GwonSuhyeon <suhyeon7175@naver.com>
2024-09-24 21:14:05 +0900

    2024.09.24

---
+++ BOJ/10707.py
@@ -0,0 +1,16 @@
+X, Y = 0, 0
+
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+P = int(input())
+
+X = P * A
+
+if P <= C:
+    Y = B
+else:
+    Y = B + (P - C) * D
+
+print(min(X, Y))
