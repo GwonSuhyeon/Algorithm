b8647867e09f84bf0eae08892704c1c11e613a11
GwonSuhyeon <suhyeon7175@naver.com>
2024-12-04 10:24:52 +0900

    2024.12.04

---
+++ BOJ/5543.py
@@ -0,0 +1,7 @@
+burger1 = int(input())
+burger2 = int(input())
+burger3 = int(input())
+drink1 = int(input())
+drink2 = int(input())
+
+print(min(burger1, burger2, burger3) + min(drink1, drink2) - 50)
