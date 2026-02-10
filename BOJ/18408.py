8025d56a13c1da8d53f5fb4da3b98bec79f817a4
GwonSuhyeon <suhyeon7175@naver.com>
2026-02-10 17:11:00 +0900

    2026.02.10

---
+++ BOJ/18408.py
@@ -0,0 +1,15 @@
+arr = list(map(int, input().split()))
+
+one = 0
+two = 0
+
+for i in arr:
+    if i == 1:
+        one += 1
+    else:
+        two += 1
+
+if one > two:
+    print(1)
+else:
+    print(2)
